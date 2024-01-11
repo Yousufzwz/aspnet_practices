08d9d7b77575fac6ceb9bbdc74255f690856d3cc
Md Yousuf <yousufzwz@gmail.com>
2024-01-11 16:40:02 +0600

    code committed v.1 (DDD & Clean Architecture setup)

---
+++ Exam1/Exam1.Application/IApplicationUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Exam1.Domain;
+using Exam1.Domain.Entities.Repositories;
+using Exam1.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDemo.Application
+{
+    public interface IApplicationUnitOfWork : IUnitOfWork
+    {
+        IBookRepository BookRepository { get; }
+    }
+}
---
+++ Exam1/Exam1.Infrastructure/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Exam1.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstDemo.Infrastructure
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Book> Books { get; set; }
+    }
+}
