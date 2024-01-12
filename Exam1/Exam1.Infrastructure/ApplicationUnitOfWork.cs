bc0c739be2ba680221eb45e760458820e0fe2bdf
Md Yousuf <yousufzwz@gmail.com>
2024-01-12 21:11:19 +0600

    code committed v.4 (database related information done)

---
+++ Exam1/Exam1.Infrastructure/ApplicationUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Exam1.Application;
+using Exam1.Domain.Entities.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam1.Infrastructure;
+
+public class ApplicationUnitOfWork : UnitOfWork, IApplicationUnitOfWork
+{
+    public ICarRepository CarRepository { get; private set; }
+
+    public ApplicationUnitOfWork(ICarRepository carRepository,
+        IApplicationDbContext dbContext) : base((DbContext)dbContext)
+    {
+        CarRepository = carRepository;
+    }
+
+}
