cedeffc790666533d58333ab59996541729e2309
Md Yousuf <yousufzwz@gmail.com>
2024-01-12 19:05:31 +0600

    code committed v.3 ('Clean Architecture' setup and necessary files are added)

---
+++ Exam1/Exam1.Application/IApplicationUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Exam1.Domain;
+using Exam1.Domain.Entities.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam1.Application
+{
+    public interface IApplicationUnitOfWork : IUnitOfWork
+    {
+        ICarRepository CarRepository { get; }
+    }
+}
---
+++ Exam1/Exam1.Infrastructure/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Exam1.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exam1.Infrastructure
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Car> Cars { get; set; }
+    }
+}
